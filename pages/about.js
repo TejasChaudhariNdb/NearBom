169181d4f8774aa52c03ef3e1c1520f38b8e0419
TejasChaudhariNdb <tejaschaudhari038@gmail.com>
2020-09-04 22:25:18 +0530

    fav

--- pages/about.js
+++ pages/about.js
@@ -3,10 +3,16 @@ import BottomNav from './components/BottomNav.js'
 import Nav from './components/Nav.js'
 import Link from 'next/link'
 import Joke from "./joke.js"
+import Head from "next/head";
 
 export default function about() {
     return (
         <>
+    <Head>
+        <title> NearBom </title>
+         <link rel="icon" href="/favicon.ico" />
+      </Head>
+
         <Nav/>
     
         <ul>
